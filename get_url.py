a7b02eb7533a41579024d4381f21b6c33105a25c
shrujancheruku <shrujancheruku@gmail.com>
2018-03-29 12:50:39 -0500

    Gitignore

--- get_url.py
+++ get_url.py
@@ -5,21 +5,19 @@ YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 
-def youtube_search(options):
+def youtube_search(query):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                     developerKey=DEVELOPER_KEY)
 
     # Call the search.list method to retrieve results matching the specified
     # query term.
     search_response = youtube.search().list(
-        q=options.q,
+        q=query,
         part='id,snippet',
-        maxResults=options.max_results
+        maxResults=1
     ).execute()
 
     videos = []
-    channels = []
-    playlists = []
 
     # Add each result to the appropriate list, and then display the lists of
     # matching videos, channels, and playlists.
@@ -27,20 +25,7 @@ def youtube_search(options):
         if search_result['id']['kind'] == 'youtube#video':
             videos.append('%s (%s)' % (search_result['snippet']['title'],
                                        search_result['id']['videoId']))
-        elif search_result['id']['kind'] == 'youtube#channel':
-            channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                         search_result['id']['channelId']))
-        elif search_result['id']['kind'] == 'youtube#playlist':
-            playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                          search_result['id']['playlistId']))
 
     print('Videos:\n', '\n'.join(videos), '\n')
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--q', help='Search term', default='Google')
-    parser.add_argument('--max-results', help='Max results', default=25)
-    args = parser.parse_args()
-    youtube_search(args)
 
